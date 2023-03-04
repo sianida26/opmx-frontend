9890fed9c15e5458c514228069166c3cde5cb170
sianida26 <chesakeselekkebo@gmail.com>
2023-03-04 20:11:23 +0700

    Added page 3

---
+++ src/pages/session2/S1P3.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function S1P3() {
+  return (
+    <main className="w-full text-slate-800 px-6 py-8 poppins flex flex-col gap-4 max-w-screen-lg mx-auto">
+        Not designed yet
+        {/* TODO: Implement Page 3 */}
+    </main>
+  )
+}
